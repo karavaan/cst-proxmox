972b8447b82b11367becfd654e5afd578a96bbfb
karavaan <quint.van.zijl@ah.nl>
2021-06-03 15:34:41 +0200

    create vm for new user

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
-import os
-
 from proxmoxer import ProxmoxAPI
 
-proxmox = ProxmoxAPI('192.168.100.2:8006', user='root@pam',
+proxmox_ip = '192.168.100.2'
+proxmox_password = 'Geheim123!'
+proxmox_node = 'my'
+default_password_for_new_accounts = "test1234!"
+
+proxmox = ProxmoxAPI(f'{proxmox_ip}:8006', user='root@pam',
                      password='Geheim123!', verify_ssl=False)
 
 
-def print_vms():
-    for vm in proxmox.cluster.resources.get(type='vm'):
-        print("{0}. {1} => {2}".format(vm['vmid'], vm['name'], vm['status']))
+def next_id():
+    try:
+        return int(proxmox.cluster.nextid.get())
+    except Exception as e:
+        print('API failure encountered.  %s' % str(e))
+        return 0
 
 
-
-def create_vm():
-    node = proxmox.nodes('my')
-    print(node)
-    node.qemu.create(
-        vmid=332,
-        name="bootstrap",
-        memory=16368,
-        sockets=2,
-        cores=2,
-        storage="local-lvm",
-        onboot=0,
-        ide2="file=iso:iso/CentOS-8.3.2011-aarch64-boot.iso,media=cdrom",
-        scsihw="virtio-scsi-pci",
-        scsi0="file=local-lvm:120",
-        net0="model=virtio,bridge=vmbr0"
-    )
+def create_user(userid, password):
+    username = userid + '@pve'
+    try:
+        proxmox.access.users.create(userid=username, password=password)
+    except Exception as e:
+        print(f'API failure encountered {e}')
+    print(f'Proxmox PVE Created User {userid}')
 
 
-# Press the green button in the gutter to run the script.
+def give_user_access_to_vm(userid, vmid):
+    try:
+        proxmox.access.acl.put(path=f'/vms/{vmid}', users=f'{userid}@pve', roles="PVEAdmin")
+    except Exception as e:
+        print(f'API failure encountered {e}')
+    print(f'Gave {userid} access to vm {vmid}')
+
+
+def create_vm(vmid):
+    try:
+        proxmox.nodes(proxmox_node).qemu.create(
+            vmid=vmid,
+            name="bootstrap",
+            memory=16368,
+            sockets=2,
+            cores=2,
+            storage="local-lvm",
+            onboot=0,
+            ide2="file=iso:iso/CentOS-8.3.2011-aarch64-boot.iso,media=cdrom",
+            scsihw="virtio-scsi-pci",
+            scsi0="file=local-lvm:120",
+            net0="model=virtio,bridge=vmbr0"
+        )
+    except Exception as e:
+        print(f'API failure encountered {e}')
+    print(f'Created vm {vmid}')
+
+
+def create_vm_for_new_user(userid):
+    vmid = next_id();
+    create_user(userid=userid, password=default_password_for_new_accounts)
+    create_vm(vmid)
+    give_user_access_to_vm(userid=userid, vmid=vmid)
+
+
 if __name__ == '__main__':
-    create_vm()
-    print_vms()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    create_vm_for_new_user('pop')
